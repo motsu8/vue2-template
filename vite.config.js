e640c7cbcd1e2867411e35c5312dbeff1e9c9bea
motsu8e <motsu159357@gmail.com>
2024-02-12 21:40:16 +0900

    vite config 変更

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,13 @@
-import vue from '@vitejs/plugin-vue2'
+/** @type {import('vite').UserConfig} */
+import vue from "@vitejs/plugin-vue2";
 
 export default {
   plugins: [vue()],
-  root: './src'
-}
+  root: "./src",
+  publicDir: "./public",
+  server: {
+    port: 5000,
+    open: true,
+    cors: false,
+  },
+};
